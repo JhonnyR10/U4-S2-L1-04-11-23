1f01247333ca9dedb95200194decf23b101f9f3c
Giovanni Longo <giovanni.longo10@icloud.com>
2023-12-04 16:16:56 +0100

    Terzo esercizio

--- src/Esercizio3/Banca.java
+++ src/Esercizio3/Banca.java
@@ -1,33 +1,38 @@
-//package Esercizio3;
-//
-//public class Banca {
-//    public static void main(String args[]) {
-//        ContoCorrente conto1 = new ContoCorrente("Grossi Mario", 20000.0);
-//
-//        System.out.println("Saldo conto: " + conto1.restituisciSaldo());
-//
-//        try {
-//            conto1.preleva(1750.5);
-//
-//            System.out.println("Saldo conto: " + conto1.restituisciSaldo());
-//        } catch (BancaException e) {
-//            System.out.println("Errore durante il prelievo: " + e);
-//            e.printStackTrace();
-//        }
-//
-//        ContoOnLine conto2 = new ContoOnLine("Rossi Luigi", 50350.0, 1500);
-//
-//        conto2.stampaSaldo();
-//
-//        try {
-//            conto2.preleva(2000);
-//
-//            conto2.stampaSaldo();
-//
-//        } catch (BancaException e) {
-//            System.out.println("Errore durante il prelievo: " + e);
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package Esercizio3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Banca {
+    private static final Logger logger = LoggerFactory.getLogger(Banca.class);
+
+    public static void main(String args[]) {
+        ContoCorrente conto1 = new ContoCorrente("Grossi Mario", 20000.0);
+
+        System.out.println("Saldo conto: " + conto1.restituisciSaldo());
+
+        try {
+            conto1.preleva(1750.5);
+
+            System.out.println("Saldo conto: " + conto1.restituisciSaldo());
+        } catch (BancaException e) {
+            System.out.println("Errore durante il prelievo: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        ContoOnLine conto2 = new ContoOnLine("Rossi Luigi", 50350.0, 1500);
+
+        conto2.stampaSaldo();
+
+        try {
+            conto2.preleva(2000);
+
+            conto2.stampaSaldo();
+
+        } catch (BancaException e) {
+            System.out.println("Errore durante il prelievo: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
 
